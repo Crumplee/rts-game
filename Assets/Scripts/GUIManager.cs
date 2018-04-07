d09d9fc60b786cd7175f5a718b704e7ed572b21b
Crumplee <raba.bence95@gmail.com>
2018-04-07 21:10:25 +0200

    multi selecting

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour {
+    public static GUIManager me;
+    public Texture2D blackTransBox;
+
+
+    // Use this for initialization
+    void Awake ()
+    {
+        me = this;	
+	}
+	
+    public Texture2D getBlackTransBox()
+    {
+        return blackTransBox;
+    }
+}
--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -6,46 +6,111 @@ public class SelectionManager : MonoBehaviour {
     public static SelectionManager me; 
 
     [SerializeField]
-    GameObject current;
+    List<GameObject> current;
+
+    GameObject firstTile = null;
+    GameObject lastTile = null;
+
+    bool drawBox = false;
 
     private void Awake()
     {
-        //findAllSpuerClassExamples();
+        current = new List<GameObject>();
     }
-    
-	
+    	
 	// Update is called once per frame
 	void Update ()
     {
         checkLeftMouseClick();
 	}
 
-    public GameObject getCurrent()
+    public List<GameObject> getCurrent()
     {
         return current;
     }
 
     public void setCurrent(GameObject s)
     {
-        current = s;
-        current.GetComponent<TileMaster>().OnSelect();
+        clearCurrent();
+        current.Add(s);
+        s.GetComponent<TileMaster>().OnSelect();
+    }
+
+    public void setCurrent(List<GameObject> sList)
+    {
+        clearCurrent();
+        current = sList;
+        foreach (GameObject o in current)
+        {
+            o.GetComponent<TileMaster>().OnSelect();
+        }
     }
 
     void clearCurrent()
     {
-        current.GetComponent<TileMaster>().OnDeSelect();
-        current = null;
+        firstTile = null;
+        lastTile = null;
+
+        foreach(GameObject o in current)
+        {
+            o.GetComponent<TileMaster>().OnDeSelect();
+        }
+        current = new List<GameObject>();
     }
 
 
     void checkLeftMouseClick()
-    {
-        if (Input.GetMouseButton(0))
+    {    
+        if(Input.GetKey(KeyCode.LeftControl)) // holding lctrl for multiple selection
+        {            
+            Debug.Log("LeftCTRL");
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Multi");
+                if (firstTile == null)
+                {
+                    Debug.Log("firstTile");
+                    selectionRaycast(ref firstTile);
+                }
+                else if (firstTile != null && lastTile != null)
+                {
+                    Vector2 startCoords = firstTile.GetComponent<TileMaster>().getCoords();
+                    Vector2 endCoords = lastTile.GetComponent<TileMaster>().getCoords();
+                    if (firstTile != null && lastTile != null)
+                    {
+                        List<GameObject> selectedTiles = MapGenerator.me.getTiles(startCoords, endCoords);
+                        setCurrent(selectedTiles);
+                        firstTile = null;
+                        lastTile = null;
+                    }
+                }
+            }
+
+            if (Input.GetMouseButton(0) == false && firstTile != null)
+            {
+                Debug.Log("lastTile");
+                drawBox = true;
+                selectionRaycast(ref lastTile);
+            }
+            else
+            {
+                drawBox = false;
+            }
+        }
+        else if (Input.GetMouseButton(0))
         {
+            firstTile = null;
+            lastTile = null;
             Debug.Log("Click");
             selectionRaycast();
+            drawBox = false;
         }
-
+        else
+        {
+            firstTile = null;
+            lastTile = null;
+            drawBox = false;
+        }
     }
 
     void selectionRaycast()
@@ -62,6 +127,80 @@ public class SelectionManager : MonoBehaviour {
         catch
         {
             Debug.Log("No valid object selected");
+        }
+    }
+
+    void selectionRaycast(ref GameObject obj) //used for multiselection
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+        RaycastHit2D raycast = Physics2D.Raycast(mousePosition, Vector2.zero, 0f);
+        try
+        {
+            GameObject hitObject = raycast.collider.gameObject;
+            Debug.Log(hitObject.name);
+            obj = hitObject;
+        }
+        catch
+        {
+            Debug.Log("No valid object selected");
+        }
+    }
+
+
+    private void OnGUI()
+    {
+        if (drawBox)
+        {
+            Vector3 startPos = Camera.main.WorldToScreenPoint(firstTile.transform.position);
+
+            Vector3 endPos = Input.mousePosition;
+
+            float width, height;
+            if (startPos.x > endPos.x)
+            {
+                width = startPos.x - endPos.x;
+            }
+            else
+            {
+                width = endPos.x - startPos.x;
+            }
+
+            if (startPos.y > endPos.y)
+            {
+                height = startPos.y - endPos.y;
+            }
+            else
+            {
+                height = endPos.y - startPos.y;
+            }
+            Rect posToDrawBox;
+
+            if (endPos.x > startPos.x)
+            {
+                if (endPos.y > startPos.y)
+                {
+                    posToDrawBox = new Rect(startPos.x, Screen.height - endPos.y, width, height);
+                }
+                else
+                {
+                    posToDrawBox = new Rect(startPos.x, Screen.height - startPos.y, width, height);
+                }
+            }
+            else
+            {
+                if (endPos.y > startPos.y)
+                {
+                    posToDrawBox = new Rect(endPos.x, Screen.height - endPos.y, width, height);
+                }
+                else
+                {
+                    posToDrawBox = new Rect(endPos.x, Screen.height - startPos.y, width, height);
+                }
+            }
+
+            GUI.Box(posToDrawBox, "");
+            //GUI.DrawTexture(posToDrawBox, GUIManager.me.getBlackTransBox());
         }
     }
 }
